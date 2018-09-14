5ad08a6d1b212d147a8cecfd15f09530984f1c38
Antonio Lassandro <lassandroan@gmail.com>
2018-09-13 21:26:00 -0500

    Store/Recall Parameters

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -176,7 +176,7 @@ BasicSynth::BasicSynth() :
     );
     output = parameters.getRawParameterValue(OUTPUT);
 
-    parameters.state = ValueTree("Basic Synth");
+    parameters.state = ValueTree("BasicSynth");
 }
 
 BasicSynth::~BasicSynth()
@@ -351,15 +351,18 @@ AudioProcessorEditor* BasicSynth::createEditor()
 
 void BasicSynth::getStateInformation(MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    auto state = parameters.copyState();
+    ScopedPointer<XmlElement> xml(state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
 void BasicSynth::setStateInformation(const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    ScopedPointer<XmlElement> xml(getXmlFromBinary(data, sizeInBytes));
+
+    if (xml.get() != nullptr)
+        if (xml->hasTagName(parameters.state.getType().toString()))
+            parameters.replaceState(ValueTree::fromXml(*xml));
 }
 
 //==============================================================================
